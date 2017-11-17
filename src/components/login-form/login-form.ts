2b558674256c02604ea29ca34c0fcbdd8129cd23
cyrusonline <online_mad@yahoo.com.hk>
2017-11-17 15:19:39 +0800

    authenticate as service
    authenticate as service

--- src/components/login-form/login-form.ts
+++ src/components/login-form/login-form.ts
@@ -1,9 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {AngularFireAuth} from 'angularfire2/auth';
 import {Account} from '../../models/account/account.interface';
 import {LoginResponse} from '../../models/login/login-response.interface';
-
+import {AuthService} from '../../providers/auth/auth.service';
 /**
  * Generated class for the LoginFormComponent component.
  *
@@ -19,20 +18,29 @@ export class LoginFormComponent {
   text: string;
 
   account = {} as Account;
-  @Output() loginStatus: EventEmitter<any>;
-  constructor(private afAuth:AngularFireAuth,private navCtrl:NavController) {
+  @Output() loginStatus: EventEmitter<LoginResponse>;
+  constructor(private auth:AuthService,private navCtrl:NavController) {
     this.loginStatus = new EventEmitter<LoginResponse>();
    
   }
 
   async login(){
-    try {
-      const result = await this.afAuth.auth.signInWithEmailAndPassword(this.account.email,this.account.password);
-      this.loginStatus.emit(result);
-    } catch (e) {
-      console.error(e);
-      this.loginStatus.emit(e);
-    }
+    const loginResponse = await this.auth.signInWithEmailAndPassword(this.account);
+    this.loginStatus.emit(loginResponse);
+
+    // try {
+    //   const result:LoginResponse = {
+    //     result:await this.afAuth.auth.signInWithEmailAndPassword(this.account.email,this.account.password)
+    //   }
+    //   this.loginStatus.emit(result);
+    // } catch (e) {
+    //   console.error(e);
+    //   const error: LoginResponse = {
+    //     error:e
+    //   }
+
+    //   this.loginStatus.emit(error);
+    // }
   }
   navigateToPage(pageName:string){
     
